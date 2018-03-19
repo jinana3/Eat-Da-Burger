2b79ac43f46f393bc3358f326640c89455de067c
jinana3 <jinali.berkeley@gmail.com>
2018-03-19 00:00:48 -0500

    heroku added and img added

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,21 @@
 //code borrowed from week 14 activity 18 catsapp
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "jinayiming0201",
+    database: "burgers_db"
+  });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
